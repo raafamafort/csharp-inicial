b05b7894e37abd47446765c24389fed696ee9882
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-23 08:49:02 -0300

    upload projeto

---
+++ RaslowApplication/Models/Alunos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RaslowApplication.Models
+{
+    public class Alunos
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+    }
+}
---
+++ RaslowApplication/Persistence/OracleDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RaslowApplication.Models;
+
+namespace RaslowApplication.Persistence
+{
+    public class OracleDbContext : DbContext
+    {
+        public DbSet<Alunos> Alunos { get; set; }
+
+        public OracleDbContext(DbContextOptions<OracleDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
